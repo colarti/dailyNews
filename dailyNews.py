7c4453146180cd80640dfda5c48826b6a8c59c90
cmolarti <cmolarti@gmail.com>
2023-12-17 10:10:14 -0500

    apinews query for input on topic and date, unicodeError, common problems

--- dailyNews.py
+++ dailyNews.py
@@ -1,18 +1,47 @@
 import os
 import requests
 import json
+import time
+from sendMessage import send_message
 
 
 API_KEY = '6a7c75fa90b540bf90732f5cd854e822'
 
-site = 'https://newsapi.org/v2/everything?q=tesla&from'\
-       '=2023-12-16&sortBy=publishedAt&apiKey=6a7c75fa90b540bf90732f5cd854e822'
+topic = 'war'
+date = time.strftime("%Y-%m-%d")
+
+site = f'https://newsapi.org/v2/everything?q={topic}&from={date}&sortBy=publishedAt&apiKey=6a7c75fa90b540bf90732f5cd854e822'
 
 data = requests.get(url=site)
 content = data.json()
 
 
-
+status = f'''\
+Subject: {topic.title()} News for {date}
+'''
+cnt = 0
 for idx, article in enumerate(content["articles"]):
     # print(article, '\n')
-    print(f'{idx+1}. {article["title"]}\n')
+    # print(f'{idx+1}. {article["title"]}\n')
+    # print(f'\t{article["description"]} \n')
+
+    try:
+        if article["title"] is None:
+            continue
+
+        if "remove" in article["title"].lower():
+            continue
+
+        status += f'''
+    {cnt+1}. {article["title"]}
+    \t{article["description"]}
+    \t{article["url"]} 
+    '''
+        cnt+=1
+    except UnicodeError:
+        continue
+
+status = status.encode('utf-8')
+
+print(status)
+send_message(status)
